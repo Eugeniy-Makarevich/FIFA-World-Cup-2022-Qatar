4e298b33a12c46a96e14e2aebccfc129f5f40945
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2022-12-27 19:30:57 +0200

    teams scrabing added

--- scraper.py
+++ scraper.py
@@ -2,10 +2,17 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import pandas
 
 #driver initialization
 driver = webdriver.Chrome()
 time.sleep(5)
+
+#make columns
+team1 = []
+team2 = []
+
+#make urls
 url = 'https://www.fifa.com/fifaplus/en/match-centre/match/17/255711/285063/400235458'
 
 #get url
@@ -15,9 +22,15 @@ time.sleep(5)
 #find STATS button and click it
 s = driver.find_element('xpath',"//*[contains(text(), 'STATS')]")
 driver.execute_script("arguments[0].click();",s)
+time.sleep(1)
 
 #make a soup
 soup = BeautifulSoup(driver.page_source,'html5lib')
 
+#get values
+#teams:
+teams =soup.find_all('div',{'class':'match-stats-tab-component_teamName__2D9h9'})
+team1.append(teams[0].string)
+team2.append(teams[1].string)
 
-print(soup.find_all('p',{'class':'ff-m-0 ff-mb-8'}))
+
