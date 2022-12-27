c0d7510766a434fbbf96a197d592463e57bf8c8f
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2022-12-27 19:50:30 +0200

    posssesions time added

--- scraper.py
+++ scraper.py
@@ -11,6 +11,8 @@ time.sleep(5)
 #make columns
 team1 = []
 team2 = []
+possesion_team1 = []
+possesion_team2 = []
 
 #make urls
 url = 'https://www.fifa.com/fifaplus/en/match-centre/match/17/255711/285063/400235458'
@@ -29,8 +31,14 @@ soup = BeautifulSoup(driver.page_source,'html5lib')
 
 #get values
 #teams:
-teams =soup.find_all('div',{'class':'match-stats-tab-component_teamName__2D9h9'})
+teams =soup.find_all('div',{'class':'match-stats-tab-component_teamName__'\
+                            '2D9h9'})
 team1.append(teams[0].string)
 team2.append(teams[1].string)
 
+#possesion
+possesion_team1.append(soup.find_all('p',{'class':'ff-m-0 single-stat-'\
+          'possession-component_numberPercentLeft__1zQaX'})[0].string)
+possesion_team2.append(soup.find_all('p',{'ff-m-0 single-stat-possession-'\
+          'component_numberPercentRight__3IfVg'})[0].string)
 
