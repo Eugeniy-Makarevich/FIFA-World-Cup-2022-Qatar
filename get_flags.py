f43ae72270f1599282ef82fe684acd22e54489aa
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2023-01-03 11:46:14 +0200

    Flags added

---
+++ get_flags.py
@@ -0,0 +1,19 @@
+#import modules
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time,pandas as pd
+
+#driver initialization
+driver = webdriver.Chrome()
+time.sleep(5)
+
+#get url and make soup
+url = 'https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022/teams'
+driver.get(url)
+time.sleep(5)
+soup = BeautifulSoup(driver.page_source,'html5lib')
+
+#get country name and flag
+country_tags = soup.find_all('div',{'class':'flag-with-info_flagCountry__Yw8QR'})
+country_list = [tag.text for tag in country_tags]
+flag_tags = soup.find_all('img',src = True)
