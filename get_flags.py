95bae8e330578f526d9266617e297eb8773037dc
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2023-02-04 19:16:57 +0200

    Add screenshot

--- get_flags.py
+++ get_flags.py
@@ -1,24 +1,25 @@
-#import modules
+# import modules
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time,re,pandas as pd
 
-#driver initialization
+# driver initialization
 driver = webdriver.Chrome()
 time.sleep(5)
 
-#get url and make soup
+# get url and make soup
 url = 'https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022/teams'
 driver.get(url)
 time.sleep(5)
 soup = BeautifulSoup(driver.page_source,'html5lib')
 
-#get country name and flag url
+# get country name and flag url
 country_tags = soup.find_all('div',{'class':'flag-with-info_flagCountry__Yw8QR'})
 country_list = [tag.text for tag in country_tags]
 flag_tags = soup.find_all('img',src = True)
 flag_list = [tag['src'] for tag in flag_tags 
              if re.findall('https://cloudinary.*',tag['src'])]
-#export to file
+
+# export to file
 df = pd.DataFrame({'country':country_list,'flag':flag_list})
 df.to_csv('country_flags.csv',index=False)
--- scraper.py
+++ scraper.py
@@ -1,13 +1,13 @@
-#import modules
+# import modules
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time,re,pandas as pd
 
-#driver initialization
+# driver initialization
 driver = webdriver.Chrome()
 time.sleep(5)
 
-#get url list
+# get url list
 base_url = 'https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/'\
        'qatar2022/scores-fixtures?'
 driver.get(base_url)
@@ -19,23 +19,23 @@ url_list = ['https://fifa.com'+url['href'] for url in
             re.search("fifaplus/en/match-centre/match/.*",url['href'])]
 
 
-#import dataframe pattern 
+# import dataframe pattern 
 df = pd.read_csv('empty_df.csv',index_col = 0)
 
-#main loop
+# main loop
 for i,url in enumerate(url_list):
     
-        #get url
+        # get url
         driver.get(url)
         time.sleep(5)
         
-        #get OVERVIEW tab content
+        # get OVERVIEW tab content
         s = driver.find_element('xpath',"//*[contains(text(), 'OVERVIEW')]")
         driver.execute_script("arguments[0].click();",s)
         time.sleep(1)
         soup = BeautifulSoup(driver.page_source,'html5lib')
     
-        #get items from OVERVIEW tab
+        # get items from OVERVIEW tab
         overview = soup.find_all('div',
                         {'class':'match-details-overview-info_info__2LEuH'})
         df['date'][i] = overview[1].text.split(',')[0]
@@ -45,44 +45,43 @@ for i,url in enumerate(url_list):
         df['referee'][i] = overview[4].text
         df['attendance'][i] = overview[8].text
         
-        #get stage
+        # get stage
         df['stage'][i] = soup.find_all('p',{'class':'ff-m-0 ff-p-0'})[0].text
-        #get penalty series score
+        
+        # get penalty series score
         penalty_score = soup.find_all('span',{'class':'show-match-score_penaltie'\
                                              'sScore__3O5O3 ff-px-16'})
         if penalty_score:
             df['penalty_series_goals_team1'][i] = penalty_score[0].text
             df['penalty_series_goals_team2'][i] = penalty_score[1].text
     
-        #get STATS tab content
+        # get STATS tab content
         s = driver.find_element('xpath',"//*[contains(text(), 'STATS')]")
         driver.execute_script("arguments[0].click();",s)
         time.sleep(1)
         soup = BeautifulSoup(driver.page_source,'html5lib')
     
-        #get items from STATS tab
-        #teams:
+        # get items from STATS tab
+        # teams:
         teams = soup.find_all('div',{'class':'match-stats-tab-component_teamName__'\
                                 '2D9h9'})
         df['team1'][i] = teams[0].text
         df['team2'][i] = teams[1].text
-        #possesion
+        # possesion
         df['possesion_team1'][i] = soup.find_all('p',{'class':'ff-m-0 single-stat-'\
                   'possession-component_numberPercentLeft__1zQaX'})[0].text
         df['possesion_team2'][i] = soup.find_all('p',{'ff-m-0 single-stat-possession-'\
                   'component_numberPercentRight__3IfVg'})[0].text
-        #other    
+        # other    
         df_columns_team1 =[value for value in df.columns.values
                            if re.findall('.*team1',value)]
         df_columns_team2 =[value for value in df.columns.values
-                           if re.findall('.*team2',value)]
-        
+                           if re.findall('.*team2',value)]   
         team1_vals = soup.find_all('p',{'class':'ff-m-0 ff-mr-20'})
         for j,val in enumerate(df_columns_team1[3:]):
-            df[val][i]=team1_vals[j].text
-        
+            df[val][i]=team1_vals[j].text     
         team2_vals = soup.find_all('p',{'class':'ff-m-0 ff-ml-20'})
         for j,val in enumerate(df_columns_team2[3:]):
             df[val][i]=team2_vals[j].text
-#export to file        
+# export to file        
 df.to_csv('FIFA WORLD CUP QATAR 2022.csv')
